90af75048bf86b1a643580ebc47edcad9321f2c6
mohibuslc <mohib_uslc@outlook.com>
2021-10-09 21:10:25 +0100

    User information Show In UI

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import { getAuth , signInWithPopup,GoogleAuthProvider } from "firebase/auth";
 
 import './button.css';
 import firebaseApp from './firebaseConfig';
+import {useState} from "react";
 
 
 const auth = getAuth(firebaseApp)
 
 function App(){  
 
+ const [user , setUser] = useState({
+
+
+   isSinedIn: false,
+   name:'',
+   email:'',
+   photo:''
+
+ });
  
   const provider = new GoogleAuthProvider();
 
@@ -20,9 +30,24 @@ function App(){
     signInWithPopup(auth, provider)
    .then(res => {
     const {displayName, photoURL,email}=res.user;
+    const signedInUser ={
 
+      isSignedIn: true,
+      name : displayName,
+      email: email,
+      Photo: photoURL
+    }
+
+    setUser(signedInUser);
+
+    
     console.log(displayName, photoURL,email)
     
+   })
+   .catch (err =>{
+     console.log(err);
+     console.log(err.message);
+
    })
   }
  
@@ -30,6 +55,12 @@ function App(){
   return (  
     <div className ="btn">
      <button  onClick={handleclick}>Sign-Up</button>
+
+     {
+
+       user.isSignedIn && <p>  Welcome , {user.name}</p>
+
+     }
      </div>
   );
 
